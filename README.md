bab513257e3f4e12f1a4cca50b74986c81700cb5
RUFFY-369 <prakarshkaushik369@gmail.com>
2022-04-19 17:46:17 +0530

    Add sol

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # GSoC-22_JdeRobot_Labyrinth_Challenge
+Solution to JdeRobot [C++ challenge](GSoC-2022-c++-test-alternative.pdf)
+
+### Usage
+
+#### Modifying labyrinth schema
+
+Please edit the files for input from anyone of the following files: `input1.txt`, `input2.txt`, `input3.txt`
+
+#### Running the code
+```bash
+mkdir build && cd build && cmake ../ && make
+./LabyrinthChallenge 
+```
+
+### Proof of working
+
+Video of the working code available here:
+
+- [Link](https://www.youtube.com/watch?v=HxIG7HFo13I)
---
+++ build/CMakeFiles/LabyrinthChallenge.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ruffy-369/GSoC'22/GSoC-22_JdeRobot_Labyrinth_Challenge/labyrinth_challenge.cpp" "/home/ruffy-369/GSoC'22/GSoC-22_JdeRobot_Labyrinth_Challenge/build/CMakeFiles/LabyrinthChallenge.dir/labyrinth_challenge.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
